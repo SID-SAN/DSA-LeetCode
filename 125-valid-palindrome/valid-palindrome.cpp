cb4a484f00e252f5246758768b6ec1f0eb52665c
Siddharth <howdy.siddharth@gmail.com>
2026-07-25 00:36:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (24.47%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str ="";
+        for(int i=0; i<s.size(); i++){
+            if(isalnum(s[i])){
+                str += tolower(s[i]);
+            }
+        }
+        for(int i=0; i<str.size()/2; i++){
+            if(str[i] != str[str.size()-i-1]) return false;
+        }
+        return true;
+    }
+};
